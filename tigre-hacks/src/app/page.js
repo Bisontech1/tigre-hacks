ee272cee748059f83d1c60b69af2e7c7c7cfecd6
David Lázaro Fernández <57787993+David-Lazaro-Fernandez@users.noreply.github.com>
2023-03-26 19:51:26 +0000

    Map section and page headers added

--- tigre-hacks/src/app/page.js
+++ tigre-hacks/src/app/page.js
@@ -3,11 +3,10 @@
 import Hero from './components/landing/Hero'
 import Nav from './components/landing/Nav'
 import { Inter } from 'next/font/google'
-import styles from './page.module.css'
 import 'leaflet/dist/leaflet.css';
-import { useClient } from 'next/client';
+import Head from 'next/head';
 
-const inter = Inter({ subsets: ['latin'] })
+
 import dynamic from 'next/dynamic';
 
 const MapWithNoSSR = dynamic(() => import('./components/landing/Map'), {
@@ -17,9 +16,11 @@ const MapWithNoSSR = dynamic(() => import('./components/landing/Map'), {
 export default function Home() {
   return (
     <>
-      <Nav/>
-      <Hero/>
-      <MapWithNoSSR/>
+
+     
+      <Nav />
+      <Hero />
+      <MapWithNoSSR />
     </>
   )
 }
