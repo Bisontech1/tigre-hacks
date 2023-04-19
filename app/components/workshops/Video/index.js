dbf8ed4279597c0aae316f87ca41de778ae1e092
David Lázaro Fernández <57787993+David-Lazaro-Fernandez@users.noreply.github.com>
2023-04-19 03:44:46 +0000

    Workshop Section

---
+++ app/components/workshops/Video/index.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import Card from '@mui/material/Card';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import Dialog from '@mui/material/Dialog';
+import YouTube from 'react-youtube';
+
+const Video = (props) => {
+  const { thumbnail, title, description, videoId, categoryColor } = props;
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <Card sx={{
+        width: 500,
+        height: 450,
+        m: 10,
+        borderRadius: 5
+      }}>
+        <CardActionArea onClick={handleClickOpen}>
+          <CardMedia
+            component="img"
+            alt={title}
+            height="250"
+            image={thumbnail}
+            title={title}
+            sx={{ borderRadius: '5px 5px 0px 0px' }}
+          />
+          <CardContent>
+            <Typography
+              variant="subtitle2"
+              sx={{
+                backgroundColor: categoryColor,
+                color: '#fff',
+                border: 0,
+                borderRadius: 50,
+                width: 100,
+                textAlign: 'center',
+                marginBottom:1
+              }}>
+              Principiante
+            </Typography>
+            <Typography gutterBottom variant="h5" component="div" sx={{ fontWeight: '500' }}>
+              {title}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {description.slice(0, 200) + "..."}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+
+      <Dialog onClose={handleClose} open={open} sx={{'& .MuiDialog-paper':{width:'80%', maxWidth:'none', height:'600px'}}}>
+        <YouTube videoId={videoId} opts={{width:'100%', height:'450px'}} />
+        <Typography variant="body1" color="text.secondary" sx={{ p: 2 }}>
+          {description}
+        </Typography>
+      </Dialog>
+    </>
+  );
+};
+
+export default Video;
---
+++ app/workshops/page.js
@@ -0,0 +1,57 @@
+"use client";
+
+import React from 'react';
+import Video from '../components/workshops/Video';
+import data from '../../public/videos/video.json';
+import Nav from '../components/landing/Nav'
+import { Box, Grid, Typography } from '@mui/material';
+const App = () => {
+    const videoData = {
+        thumbnail: 'https://cdn2.unrealengine.com/c4-s2-battle-pass-1920x1080-603842488c24.jpg',
+        title: 'Mi Video',
+        description: 'Esta es la descripción de mi video',
+        videoId: 'abcdefg',
+    };
+
+
+    return (
+        <Box >
+            <Box sx={{ marginBottom: 20 }}>
+                <Nav />
+            </Box>
+            <Box sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                '& .MuiTypography-root': { textAlign: 'center', width: '80%' }
+            }}>
+                <Typography variant="h2" sx={{ fontWeight: '600' }}>Workshops</Typography>
+                <Typography variant="h5" >It's dangerous to go alone take this!</Typography>
+                <Typography variant="h6">Ya seas el hacker más malevolo y norteño del condado o eres solo un compita que quiere ver que onda con el hackathon y aprender un rato, este lugar es para ti</Typography>
+            </Box>
+
+            <Grid
+                container
+                justifyContent="center"
+                alignItems="center"
+                direction="row"
+            >
+                {data.map((item) => {
+                    return (
+                        <Grid item>
+                            <Video
+                                thumbnail={item.src}
+                                title={item.name}
+                                description={item.description}
+                                categoryColor={item.color}
+                                videoId="dQw4w9WgXcQ"
+                            />
+                        </Grid>
+                    )
+                })}
+            </Grid>
+        </Box>
+    );
+};
+
+export default App;
