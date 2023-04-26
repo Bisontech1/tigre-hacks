c7cac95c7aa51b75b7fa69f77eb6389034b18260
Fernando Maldonado <f_maldonado6@hotmail.com>
2023-04-26 02:52:36 -0600

    Se añaden más traducciones

--- app/components/register/WelcomeDialog/index.js
+++ app/components/register/WelcomeDialog/index.js
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
-import Dialog from '@mui/material/Dialog';
-import Button from '@mui/material/Button';
-import Fade from '@mui/material/Fade';
-import { Typography } from '@mui/material';
-import './index.css'
+import React, { useState } from "react";
+import Dialog from "@mui/material/Dialog";
+import Button from "@mui/material/Button";
+import Fade from "@mui/material/Fade";
+import { Typography } from "@mui/material";
+import "./index.css";
+import { useTranslationContext } from "contexts";
 
 const WelcomeDialog = () => {
+  const { language, setLanguage } = useTranslationContext();
   const [open, setOpen] = useState(true);
 
   const handleClose = () => {
@@ -13,18 +15,24 @@ const WelcomeDialog = () => {
   };
 
   return (
-    <Dialog
-      open={open}
-      onClose={handleClose}
-      className="dialog"
-    >
+    <Dialog open={open} onClose={handleClose} className="dialog">
       <Fade in={open}>
         <div className="paper">
-          <Typography variant="h5" sx={{ fontWeight: '600', color: '#424242' }}>¡Bienvenido Hacker 🐯!</Typography>
-          <Typography variant="h6" sx={{ color: '#424242' }}>Estamos emocionados de tenerte aquí</Typography>
-          <Typography variant="subtitle1" sx={{ color: '#000000de' }}>Nos complace darte la bienvenida a nuestro hackathon y agradecemos tu interés en participar. Estamos seguros de que tendrás una experiencia única y emocionante.</Typography>
-          <Typography variant="subtitle1" sx={{ color: '#000000de' }}>Antes de comenzar necesitamos registrar algunos de tus datos para tu experiencia como hacker sea la mejor.</Typography>
-          <Button variant="contained" color="primary" onClick={handleClose}>Empezar</Button>
+          <Typography variant="h5" sx={{ fontWeight: "600", color: "#424242" }}>
+            {language?.getString("register.welcome.title")}
+          </Typography>
+          <Typography variant="h6" sx={{ color: "#424242" }}>
+            {language?.getString("register.welcome.subtitle")}
+          </Typography>
+          <Typography variant="subtitle1" sx={{ color: "#000000de" }}>
+            {language?.getString("register.welcome.description1")}
+          </Typography>
+          <Typography variant="subtitle1" sx={{ color: "#000000de" }}>
+            {language?.getString("register.welcome.description2")}
+          </Typography>
+          <Button variant="contained" color="primary" onClick={handleClose}>
+            {language?.getString("register.welcome.start")}
+          </Button>
         </div>
       </Fade>
     </Dialog>
